5c9b2bb2e7c6aedc73b6a98147c8864983f86a83
Nazmul <nazmulkabIr2006@gmail.com>
2024-11-03 00:12:23 +0600

    papers page added

--- assets/scripts/base-components.js
+++ assets/scripts/base-components.js
@@ -34,22 +34,22 @@
 
 
 class snapsTopNav extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `<header>
+  connectedCallback() {
+    this.innerHTML = `<header>
     <div class="container">
-      <a href="https://snapsedu.com/" class="logo"><img src="https://snapsedu.com/images/logo.png" alt="Logo"
+      <a href="https://snapsedu.com/" class="logo"><img src="https://snapsedu.com/assets/images/logo.pngsnapsedu.com/images/logo.png" alt="Logo"
           class="logo"></a>
       <nav>
         <ul>
           <li><a href="https://snapsedu.com/">Home</a></li>
-          <li><a href="https://snapsedu.com/question-papers/index.html">Question Papers</a></li>
-          <li><a href="https://snapsedu.com/notes/notes.html">Notes</a></li>
+          <li><a href="https://snapsedu.com/papers.html">Question Papers</a></li>
+          <li><a href="https://snapsedu.com/notes.html">Notes</a></li>
           <li><a href="https://snapsedu.com/contact.html">Contact us</a></li>
         </ul>
       </nav>
     </div>
   </header>`
-    }
+  }
 }
 
 customElements.define('snaps-top-nav', snapsTopNav);
@@ -57,8 +57,8 @@ customElements.define('snaps-top-nav', snapsTopNav);
 
 
 class snapsBottomNav extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
+  connectedCallback() {
+    this.innerHTML = `
           <footer>
         <div class="container">
             <p>&copy; 2024 SNAPS Education. All Rights Reserved.</p>
@@ -66,7 +66,7 @@ class snapsBottomNav extends HTMLElement {
                     href="https://snapsedu.com/about.html">About</a></p>
         </div>
     </footer>`
-    }
+  }
 }
 
 customElements.define('snaps-bottom-nav', snapsBottomNav);
